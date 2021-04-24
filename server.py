2b28fd966b65bb10321f08b36ab8cb8f4458d2a4
Rachel <rachelsuk97@gmail.com>
2021-04-24 03:33:38 +0000

    connect to yelpAPI and get 10 businesses in view fxn yelphelper_session_setup()

--- server.py
+++ server.py
@@ -30,11 +30,15 @@ def yelphelper_session_setup():
     price = request.form.get("price")
     min_rating = request.form.get("min_rating")
 
-    # create yelphelper_session using form data and save to database
+    # create YelpHelperSession object using form data and save to database
     yelphelper_session = YelpHelperSession(date=date, location=location, term=search_term,
                                            price=price, min_rating=min_rating)
     db.session.add(yelphelper_session)
     db.session.commit()
+
+    # connect to yelp API and get 10 restaurants that fit form data criteria
+    businesses = yelp_api.business_search(
+        location, search_term, price, min_rating)
 
     return "yelphelper session setup"
 
