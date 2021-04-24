61ab4d8df2da8706d99e7b4b9d488835c5bab327
Rachel <rachelsuk97@gmail.com>
2021-04-24 03:17:44 +0000

    Set up server.py file

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, redirect, url_for
+import model
+from model import db, User, YelpHelperSession, UserYelpHelperSession, Business, Score
+import yelp_api
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+app.secret_key = os.environ['APP_KEY']
+
+
+if __name__ == '__main__':
+    model.connect_to_db(app)
+    app.run(debug=True, host='0.0.0.0')
