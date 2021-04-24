7988beee3e026c5923eff31f15bc31517a6756e4
Rachel <rachelsuk97@gmail.com>
2021-04-24 03:18:24 +0000

    add view fxns for homepage and form

--- server.py
+++ server.py
@@ -9,6 +9,16 @@ app = Flask(__name__)
 app.secret_key = os.environ['APP_KEY']
 
 
+@app.route('/')
+def homepage():
+    return render_template('index.html')
+
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+
 if __name__ == '__main__':
     model.connect_to_db(app)
     app.run(debug=True, host='0.0.0.0')
