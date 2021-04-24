0b331f7c8014bf8874f0060c592b4707611bbac7
Rachel <rachelsuk97@gmail.com>
2021-04-24 03:39:47 +0000

    Added price and min_rating to payload

--- yelp_api.py
+++ yelp_api.py
@@ -8,7 +8,8 @@ def business_search(location, search_term, price=None, min_rating=None):
     url = 'https://api.yelp.com/v3/businesses/search'
     # Use form data from the user to populate any search parameters
     headers = {'Authorization': 'Bearer %s' % YELP_KEY}
-    payload = {'term': search_term, 'location': location}
+    payload = {'term': search_term, 'location': location,
+               'price': price, 'min_rating': min_rating}
     # Make a request to the Event Search endpoint to search for events
     res = requests.get(url, params=payload, headers=headers)
     search_results = res.json()
