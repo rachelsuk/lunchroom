260dc8a0dc29df1f83287733d71b689135324e64
Rachel <rachelsuk97@gmail.com>
2021-04-24 03:13:32 +0000

    Added yelp_api.py with business_search fxn

---
+++ yelp_api.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+YELP_KEY = os.environ['YELP_KEY']
+
+
+def business_search(location, search_term, price=None, min_rating=None):
+    url = 'https://api.yelp.com/v3/businesses/search'
+    # Use form data from the user to populate any search parameters
+    headers = {'Authorization': 'Bearer %s' % YELP_KEY}
+    payload = {'term': search_term, 'location': location}
+    # Make a request to the Event Search endpoint to search for events
+    res = requests.get(url, params=payload, headers=headers)
+    search_results = res.json()
+    return search_results['businesses']
