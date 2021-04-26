ec7a8fe5d9f456cd06ae50954d83516a2c304862
Rachel <rachelsuk97@gmail.com>
2021-04-26 08:16:23 +0000

    Initial Commit

--- static/js/components/loginForm.jsx
+++ static/js/components/loginForm.jsx
@@ -1,9 +1,28 @@
 function LoginForm(props) {
+
+    function loginHandler(evt) {
+        evt.preventDefault();
+
+        const formData = {
+            'email': document.querySelector('#login-form').elements.email.value,
+            'password': document.querySelector('#login-form').elements.password.value
+        };
+        $.post('/login', formData, (res) => {
+            if (res.message === "success") {
+                props.onSuccess();
+            }
+            else {
+                props.setErrorMessage(res.message);
+            }
+            
+        });
+
+    }
     
     return (
         <React.Fragment>
             <h2>Login</h2>
-            <form action="/login" method="POST">
+            <form onSubmit={loginHandler} id="login-form">
                 Email <input type="text" name="email" />
                 Password <input type="password" name="password" />
                 <input type="submit" />
