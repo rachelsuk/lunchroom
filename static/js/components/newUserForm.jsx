b35c24d1cc24752032278712e181ea097f482f40
Rachel <rachelsuk97@gmail.com>
2021-04-26 08:17:19 +0000

    added registerUserHandler fxn

--- static/js/components/newUserForm.jsx
+++ static/js/components/newUserForm.jsx
@@ -1,8 +1,31 @@
 function NewUserForm(props) {
+
+    function registerUserHandler(evt) {
+        evt.preventDefault();
+
+        const newUserForm = document.querySelector('#new-user-form').elements;
+
+        const formData = {
+            'fname': newUserForm.fname.value,
+            'lname': newUserForm.lname.value,
+            'phone': newUserForm.phone.value,
+            'email': newUserForm.email.value,
+            'password': newUserForm.password.value
+        };
+        $.post('/new-user', formData, (res) => {
+            if (res.message === 'success') {
+                props.onSuccess();
+            }
+            else {
+                props.setErrorMessage(res.message);
+            }
+        });
+    }
+
     return (
         <React.Fragment>
             <h2>Create an Account</h2>
-            <form action="/new-user" method="POST">
+            <form onSubmit={registerUserHandler} id="new-user-form">
                 First Name <input type="text" name="fname" />
                 Last Name <input type="text" name="lname" />
                 Phone Number <input type="text" name="phone" />
