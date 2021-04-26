71661ace389654bd01b972e24bef2e7f33a28dbb
Rachel <rachelsuk97@gmail.com>
2021-04-26 08:18:13 +0000

    Added errorMessage component and onSuccess fxn

--- static/js/login.jsx
+++ static/js/login.jsx
@@ -1,11 +1,21 @@
 function YelpHelper(props) {
+    const [errorMessage, setErrorMessage] = React.useState(null);
+
+
+    const onSuccess = async () => {
+        window.location.replace("/form");
+    }
+
     return (
         <React.Fragment>
-            <NewUserForm />
-            <LoginForm />
+            {errorMessage ? <ErrorMessage errorMessage={errorMessage} /> : null}
+            <NewUserForm setErrorMesssage={setErrorMessage} onSuccess={onSuccess} />
+            <LoginForm setErrorMessage={setErrorMessage} onSuccess={onSuccess} />
         </React.Fragment>
     );
 }
+
+
 
 ReactDOM.render(
     <YelpHelper />,
