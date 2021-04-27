aed525863c3e56a2e8e81833bdf391dc8dc2ad59
Rachel <rachelsuk97@gmail.com>
2021-04-27 05:21:08 +0000

    added hyperlink to yelp page and image

--- static/js/components/business.jsx
+++ static/js/components/business.jsx
@@ -2,7 +2,10 @@ function Business(props) {
     const business = props.business;
     return (
         <React.Fragment>
-            <b className='business-name'>{business.name}</b>
+            <a className='business-name' href={business.url} target="_blank">{business.name}</a>
+            <div>
+                <img src={`${business.image_url}`} width="200" height="200"/>
+            </div>
             <ul className='business-details'>
                 <li>Number of Yelp Reviews: {business.review_count}</li>
                 <li>Number of Yelp Stars: {business.yelp_rating}</li>
