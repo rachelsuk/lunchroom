3d1f92a203d137e75d3442b2c067caab7fa616bb
Rachel <rachelsuk97@gmail.com>
2021-04-30 03:46:21 +0000

    refactored code and added toggle login/logout feature

--- static/js/components/header.jsx
+++ static/js/components/header.jsx
@@ -1,5 +1,21 @@
-const Link =  window.ReactRouterDOM.Link;
-
 function Header(props) {
-    return <Link to='/login'>Login.</Link>
+    const [loggedIn, setLoggedIn] = React.useState(false);
+
+    React.useEffect(() => {
+        $.get('/check-login.json', (res) => {
+            setLoggedIn(res.logged_in);
+        });
+    },[]);
+
+    function processLogOut() {
+        $.get('/process-logout', (res) => {
+            setLoggedIn(false);
+        });
+    }
+
+    return (
+        <React.Fragment>
+           {loggedIn ? <button onClick={processLogOut}>Logout</button> : <a href={`/login?url=${props.url}`}>Login</a>}
+        </React.Fragment>
+        );
 }
