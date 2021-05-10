41a51e16b18d8f5c99c1c77f002df325e011249c
Rachel <rachelsuk97@gmail.com>
2021-05-10 05:36:29 +0000

    added redirectLogIn function

--- static/js/components/header.jsx
+++ static/js/components/header.jsx
@@ -13,9 +13,13 @@ function Header(props) {
         });
     }
 
+    function redirectLogIn() {
+        window.location.replace(`/login?url=${props.url}`);
+    }
+
     return (
         <React.Fragment>
-           {loggedIn ? <button onClick={processLogOut}>Logout</button> : <a href={`/login?url=${props.url}`}>Login</a>}
+           {loggedIn ? <button onClick={processLogOut}>Logout</button> : <button onClick={redirectLogIn}>Login</button>}
         </React.Fragment>
         );
 }
