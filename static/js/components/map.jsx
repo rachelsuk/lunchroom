ce970ed9f0c1e325297a88eda12c4cd96c283008
Rachel <rachelsuk97@gmail.com>
2021-05-11 05:53:41 +0000

    loaded business data into markers

--- static/js/components/map.jsx
+++ static/js/components/map.jsx
@@ -1,30 +1,29 @@
+// https://engineering.universe.com/building-a-google-map-in-react-b103b4ee97f1
+// https://stackoverflow.com/questions/15719951/auto-center-map-with-multiple-markers-in-google-maps-api-v3
+
 function GoogleMap(props) {
     const businesses = props.businesses;
 
     React.useEffect(() => {
 
-        const sfBayCoords = {
-            lat: 37.601773,
-            lng: -122.202870
-          };
-
         const googleMap = new google.maps.Map(
-            document.getElementById("google-map"),
-            {
-                center: sfBayCoords,
-                zoom: 11
-            })
-        // const bounds = new google.maps.LatLngBounds();
+            document.getElementById("google-map"));
+
+        const bounds = new google.maps.LatLngBounds();
 
 
-        const markers = [];
-
-        for (const business of businesses) {
-            markers.push(new google.maps.Marker({
+        const businessMarkers = [];
+        for (const [index, business] of businesses.entries()) {
+            let number = index + 1
+            businessMarkers.push(new google.maps.Marker({
                 position: {lat: business.lat, lng: business.lng },
+                title: business.name,
                 map: googleMap,
+                image_url: business.image_url,
+                url: business.url,
+                label: number.toString(),
                 icon: {  // custom icon
-                    url: '/static/img/marker.svg',
+                    url: '/static/img/hamburger.png',
                     scaledSize: {
                       width: 30,
                       height: 30
@@ -33,13 +32,33 @@ function GoogleMap(props) {
             }));
         }
 
-        // for (const marker of markers) {
-        // //     bounds.extend(marker.position);
-        // }
+        for (const marker of businessMarkers) {
 
-        // googleMap.fitBounds(bounds);
-        // googleMap.panToBounds(bounds);
-    }, []);
+            bounds.extend(marker.position);
+
+            const markerInfo = (`
+                <h1><a className='business-name' href=${marker.url} target="_blank">${marker.title}</a></h1>
+                <img src=${marker.image_url} width="300" height="300" />
+                <p>
+                Located at: <code>${marker.position.lat()}</code>,
+                <code>${marker.position.lng()}</code>
+                </p>
+            `);
+        
+            const infoWindow = new google.maps.InfoWindow({
+                content: markerInfo,
+                maxWidth: 400
+            });
+        
+            marker.addListener('click', () => {
+                infoWindow.open(googleMap, marker);
+            });
+            }
+
+
+        googleMap.fitBounds(bounds);
+        googleMap.panToBounds(bounds);
+    });
 
    return (
        <div id="google-map" style={{ width: '800px', height: '600px' }} />
