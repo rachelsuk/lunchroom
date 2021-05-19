4a16a34afefbce81c00a9c57efe80972d01d6c23
Rachel <rachelsuk97@gmail.com>
2021-05-19 06:06:47 +0000

    changed ajax call from /get-users-locations.json to /retrieve-users-locations.json

--- static/js/result.jsx
+++ static/js/result.jsx
@@ -4,18 +4,18 @@ function ResultsContainer(props) {
     const businessesInfo = [];
 
     React.useEffect(() => {
-		$.get('/results.json', (result) => {
-			setBusinessesResults(result.total_scores);
-		});
-        $.get('/get-users-locations.json', (result) => {
+        $.get('/results.json', (result) => {
+            setBusinessesResults(result.total_scores);
+        });
+        $.get('/retrieve-users-locations.json', (result) => {
             setUsersLocations(result.users_locations);
         });
-	},[]);
+    }, []);
 
     for (const business of businessesResults) {
         businessesInfo.push(
             <div className="business" key={business.alias}>
-                <Business business={business}/>
+                <Business business={business} />
                 <p>{business.total_score}</p>
             </div>
         );
@@ -24,7 +24,7 @@ function ResultsContainer(props) {
     return (
         <React.Fragment>
             <a href={'/'}>Return to Homepage</a>
-            <GoogleMap businesses = {businessesResults} usersLocations = {usersLocations}/>
+            <GoogleMap businesses={businessesResults} usersLocations={usersLocations} />
             <div>{businessesInfo}</div>
         </React.Fragment>
     )
