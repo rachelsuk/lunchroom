355a2edb85d2350d2fb3860d68dcef4a544716a4
Rachel <rachelsuk97@gmail.com>
2021-05-26 07:14:58 +0000

    show restaurants on results page one at a time

--- static/js/result.jsx
+++ static/js/result.jsx
@@ -1,6 +1,7 @@
 function ResultsContainer(props) {
     const [businessesResults, setBusinessesResults] = React.useState([]);
     const [usersLocations, setUsersLocations] = React.useState([]);
+    const [businessIndex, setBusinessIndex] = React.useState(0)
     const businessesInfo = [];
 
     React.useEffect(() => {
@@ -15,17 +16,28 @@ function ResultsContainer(props) {
     for (const business of businessesResults) {
         businessesInfo.push(
             <div className="business" key={business.alias}>
+                <h2>{businessIndex + 1}.</h2>
                 <Business business={business} />
                 <p>{business.total_score}</p>
             </div>
         );
     }
 
+    function showPreviousBusiness() {
+        setBusinessIndex(businessIndex - 1);
+    }
+
+    function showNextBusiness() {
+        setBusinessIndex(businessIndex + 1);
+    }
+
     return (
         <React.Fragment>
             <a href={'/'}>Return to Homepage</a>
             <GoogleMap businesses={businessesResults} usersLocations={usersLocations} />
-            <div>{businessesInfo}</div>
+            {businessesInfo ? <div>{businessesInfo[businessIndex]}</div> : null}
+            {businessIndex != 0 ? <button onClick={showPreviousBusiness}>Back</button> : null}
+            {businessIndex < (businessesInfo.length - 1) ? <button onClick={showNextBusiness}>Next</button> : null}
         </React.Fragment>
     )
 }
