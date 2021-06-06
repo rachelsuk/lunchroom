80ccad8085146471f9948169db86eeb6f8a5af3d
Rachel <rachelsuk97@gmail.com>
2021-06-06 08:22:15 +0000

    moved buttons to inside business component

--- static/js/components/business.jsx
+++ static/js/components/business.jsx
@@ -2,6 +2,7 @@ function Business(props) {
     const business = props.business;
     const showSaveButton = props.showSaveButton;
     const setErrorMessage = props.setErrorMessage;
+    const showAddButton = props.showAddButton;
 
     function addToSavedBusinesses() {
         $.post('/add-to-saved-businesses.json', { 'business-alias': business.alias }, (res) => {
@@ -13,17 +14,38 @@ function Business(props) {
         })
     }
 
+
+    function addBusiness(evt) {
+        let id = evt.target.parentElement.parentElement.id
+        $.post('/add-saved-business-to-yp-session.json', { 'saved-business-id': id }, (res) => {
+            if (res.msg == "success") {
+                props.setErrorMessage("Business has been added to the session.")
+            } else if (res.msg == "already added") {
+                props.setErrorMessage("Business has already been added to the session.")
+            }
+        });
+    }
+
     return (
         <React.Fragment>
-            <a className='business-name' href={business.url} target="_blank">{business.name}</a>
-            <div>
-                <img src={`${business.image_url}`} width="200" height="200" />
+            <div className="business-container" id={business.saved_business_id}>
+                <div className="business-info">
+                    <a className='business-name' id={'business-name' + business.alias} href={business.url} target="_blank">{business.name}</a>
+                    <div>
+                        <img src={`${business.image_url}`} width="200" height="200" />
+                    </div>
+                    {(business.review_count && business.yelp_rating) && <ul className='business-details'>
+                        <li>Number of Yelp Reviews: {business.review_count}</li>
+                        <li>Number of Yelp Stars: {business.yelp_rating}</li>
+                    </ul>}
+                </div>
+                <div className="business-btns">
+                    {showSaveButton && <button onClick={addToSavedBusinesses}>Add to my saved restaurants.</button>}
+                    {showAddButton && <button className="btn add-restaurant-btn" onClick={addBusiness}>Add Restaurant</button>}
+                </div>
             </div>
-            <ul className='business-details'>
-                <li>Number of Yelp Reviews: {business.review_count}</li>
-                <li>Number of Yelp Stars: {business.yelp_rating}</li>
-            </ul>
-            {showSaveButton ? <button onClick={addToSavedBusinesses}>Add to my saved businesses.</button> : null}
-        </React.Fragment>
+            <hr />
+
+        </React.Fragment >
     );
 }
