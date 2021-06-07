a4c3773df08eda471378b12faa06b5488eb2768a
Rachel Suk <rachelsuk97@gmail.com>
2021-06-07 01:26:45 -0700

    added baianat icons attribution

--- static/js/components/footer.jsx
+++ static/js/components/footer.jsx
@@ -2,6 +2,7 @@ function Footer(props) {
     return (
         <div id="footer">
             <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            <div>Icons made by <a href="https://www.flaticon.com/authors/baianat" title="Baianat">Baianat</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </div>
     );
 }
