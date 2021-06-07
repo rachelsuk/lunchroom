6f69a6a4c7a5e342862885af96370143be7aedb9
Rachel Suk <rachelsuk97@gmail.com>
2021-06-07 01:30:51 -0700

    converted business score dropdown to radio btns

--- static/js/quiz.jsx
+++ static/js/quiz.jsx
@@ -33,9 +33,12 @@ function QuizContainer(props) {
     function submitHandler(evt) {
         evt.preventDefault();
 
+
+
         const businessScoreInput = {
             'business-index': businessIndex,
-            'score': document.querySelector('#business-score').elements.business_score.value
+            'score': $(".business-score:checked").val()
+            // 'score': document.querySelector('#business-score').elements.business_score.value
         };
         $.post('/save-score', businessScoreInput, (res) => {
             if (businessIndex < businesses.length - 1) {
@@ -43,7 +46,7 @@ function QuizContainer(props) {
             }
             else {
                 fetch('/user-completed', { method: 'POST' });
-                window.location.replace("/waiting-room-end");
+                window.location.assign("/waiting-room-end");
             }
         });
 
@@ -51,8 +54,8 @@ function QuizContainer(props) {
 
     return (
         <React.Fragment>
-            {businesses.length > 0 ? <Quiz businesses={businesses} businessIndex={businessIndex}
-                usersLocations={usersLocations} submitHandler={submitHandler} /> : null}
+            {businesses.length > 0 && <Quiz businesses={businesses} businessIndex={businessIndex}
+                usersLocations={usersLocations} submitHandler={submitHandler} />}
         </React.Fragment>
     );
 }
@@ -67,19 +70,61 @@ function Quiz(props) {
     return (
         <React.Fragment>
             {errorMessage ? <ErrorMessage errorMessage={errorMessage} /> : null}
-            <div id='business-quiz'>
-                <Business business={business} showSaveButton={true} setErrorMessage={setErrorMessage} />
-                <form id='business-score' onSubmit={props.submitHandler}>
-                    <select name="business_score">
-                        <option value="0">Absolutely not</option>
-                        <option value="2">Maybe</option>
-                        <option value="3">Looks Good!</option>
-                        <option value="5">This is the place for sure!!!</option>
-                    </select>
-                    <input type='submit' />
-                </form>
+            <div id='business-quiz-container' className="center">
+                <div id='google-map-container'>
+                    {/* <QuizGoogleMap business={business} businessIndex={businessIndex} usersLocations={usersLocations} /> */}
+                </div>
+                <div id='business-quiz'>
+                    <Business business={business} showSaveButton={true} setErrorMessage={setErrorMessage} />
+                    <form id='business-score' onSubmit={props.submitHandler}>
+                        <div id='business-score-container'>
+                            <div id="business-score-label">Do you want to eat here?</div>
+                            <div className="business-score-radio-group">
+                                <input type="radio" className="business-score radio" name="business-score" value="0" id="business-score-0" />
+                                {/* <label className="business-score-label radio-label" htmlFor="business-score-0">Absolutely not</label> */}
+                                <label className="business-score-label radio-label" htmlFor="business-score-0">
+                                    <img className="business-score-icon" src="/static/img/sad.png"></img>
+                                </label>
+                                <span className="tooltiptext">Absolutely Not</span>
+                            </div>
+                            <div className="business-score-radio-group">
+                                <input type="radio" className="business-score radio" name="business-score" value="2" id="business-score-2" />
+                                {/* <label className="business-score-label radio-label" htmlFor="business-score-2">Maybe</label> */}
+                                <label className="business-score-label radio-label" htmlFor="business-score-2">
+                                    <img className="business-score-icon" src="/static/img/neutral.png"></img>
+                                </label>
+                                <span className="tooltiptext">Maybe</span>
+
+                            </div>
+                            <div className="business-score-radio-group">
+                                <input type="radio" className="business-score radio" name="business-score" value="3" id="business-score-3" />
+                                {/* <label className="business-score-label radio-label" htmlFor="business-score-3">Looks Good!</label> */}
+                                <label className="business-score-label radio-label" htmlFor="business-score-3">
+                                    <img className="business-score-icon" src="/static/img/happy.png"></img>
+                                </label>
+                                <span className="tooltiptext">Looks Good!</span>
+                            </div>
+                            <div className="business-score-radio-group">
+                                <input type="radio" className="business-score radio" name="business-score" value="5" id="business-score-5" />
+                                {/* <label className="business-score-label radio-label" htmlFor="business-score-5">This is the place for sure!!</label> */}
+                                <label className="business-score-label radio-label" htmlFor="business-score-5">
+                                    <img className="business-score-icon" src="/static/img/very-happy.png"></img>
+                                </label>
+                                <span className="tooltiptext">This is the place for sure!</span>
+                            </div>
+                            {/* <select name="business_score">
+                            <option value="0">Absolutely not</option>
+                            <option value="2">Maybe</option>
+                            <option value="3">Looks Good!</option>
+                            <option value="5">This is the place for sure!!!</option>
+                        </select> */}
+                            {/* <input type='submit' /> */}
+                        </div>
+                        <button className="btn submit-btn" id="submit-business-score-btn" onClick={props.submitHandler}>Go to Next Restaurant</button>
+                    </form>
+                </div>
             </div>
-            <QuizGoogleMap business={business} businessIndex={businessIndex} usersLocations={usersLocations} />
+
         </React.Fragment>
     )
 }
