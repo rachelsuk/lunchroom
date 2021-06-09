1b69c1d8249f820923fa36d54473f50da60c283a
Rachel Suk <rachelsuk97@gmail.com>
2021-06-08 23:30:48 -0700

    converted error message component into a pop up msg

--- static/js/components/errorMessage.jsx
+++ static/js/components/errorMessage.jsx
@@ -1,7 +1,20 @@
 function ErrorMessage(props) {
     const errorMessage = props.errorMessage;
+
+    React.useEffect(() => {
+        if (errorMessage) {
+            const popUp = document.querySelector(".pop-up-msg");
+
+            // Add the "show" class to DIV
+            popUp.className = "show pop-up-msg";
+
+            // After 3 seconds, remove the show class from DIV
+            setTimeout(function () { popUp.className = popUp.className.replace("show pop-up-msg", "pop-up-msg"); }, 3000);
+
+        }
+    }, [errorMessage]);
     return (
-        <div>{errorMessage}</div>
+        <div className="pop-up-msg">{errorMessage}</div>
     );
 
 }
