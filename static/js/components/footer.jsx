6f0ce59e2dd5f046f8d023ecc41daa5789da3e98
Rachel Suk <rachelsuk97@gmail.com>
2021-06-08 23:31:32 -0700

    added attribution for homepage vector img

--- static/js/components/footer.jsx
+++ static/js/components/footer.jsx
@@ -3,6 +3,7 @@ function Footer(props) {
         <div id="footer">
             <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
             <div>Icons made by <a href="https://www.flaticon.com/authors/baianat" title="Baianat">Baianat</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+            <a href="https://www.vecteezy.com/free-vector/vector">Vector Vectors by Vecteezy</a>
         </div>
     );
 }
