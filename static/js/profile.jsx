1d2435d471b1850662972e79fc24840c1af19ac8
Rachel <rachelsuk97@gmail.com>
2021-06-16 07:19:23 +0000

    added styling and added edit profile tab

--- static/js/profile.jsx
+++ static/js/profile.jsx
@@ -1,5 +1,6 @@
 function Profile(props) {
     const [savedBusinesses, setSavedBusinesses] = React.useState([])
+    const [showSavedBusinesses, setShowSavedBusinesses] = React.useState(true);
     const errorMsgRef = React.useRef();
     React.useEffect(() => {
         $.get('/get-saved-businesses.json', (res) => {
@@ -11,7 +12,15 @@ function Profile(props) {
         <React.Fragment>
             <Header />
             <ErrorMessage ref={errorMsgRef} />
-            {savedBusinesses.length > 0 ? <SavedBusinesses savedBusinesses={savedBusinesses} setSavedBusinesses={setSavedBusinesses} errorMsgRef={errorMsgRef} /> : <div>no saved businesses</div>}
+            <div id="profile-sidebar">
+                <div>My Profile</div>
+                <div><button className="btn" onClick={() => setShowSavedBusinesses(true)}>Saved Businesses</button></div>
+                <div><button className="btn" onClick={() => setShowSavedBusinesses(false)}>Edit Profile</button></div>
+            </div>
+            {showSavedBusinesses ?
+                <SavedBusinesses savedBusinesses={savedBusinesses} setSavedBusinesses={setSavedBusinesses} errorMsgRef={errorMsgRef} />
+                : <EditProfile />
+            }
         </React.Fragment>
     );
 }
@@ -21,27 +30,44 @@ function SavedBusinesses(props) {
     const setSavedBusinesses = props.setSavedBusinesses;
     const businessesInfo = [];
 
-    function removeBusiness(evt) {
-        let id = evt.target.parentElement.id
-        $.post('/remove-saved-business.json', { 'saved-business-id': id }, (res) => {
-            if (res.msg == "success") {
-                props.errorMsgRef.current.showErrorMessage("Business has been removed from saved businesses.")
-                $.get('/get-saved-businesses.json', (res) => {
-                    setSavedBusinesses(res.saved_businesses);
-                })
-            }
-        });
-    }
+
 
     for (const [index, business] of savedBusinesses.entries()) {
         businessesInfo.push(
             <div className="business" index={index} id={business.saved_business_id} key={business.alias}>
-                <Business business={business} showSaveButton={false} />
-                <button onClick={removeBusiness}>Remove</button>
+                <Business business={business} showSaveButton={false} showRemoveButton={true} errorMsgRef={props.errorMsgRef} setSavedBusinesses={setSavedBusinesses} />
+                <hr />
             </div>
         );
     }
-    return <div>{businessesInfo}</div>
+    return (
+        <div id="profile-saved-businesses" className="center">
+            <h1 id="profile-saved-businesses-header" className="profile-headers">Saved Restaurants</h1>
+            {savedBusinesses.length > 0 ? <div>{businessesInfo}</div> : <div>no saved businesses</div>}
+        </div>
+    )
+}
+
+function EditProfile(props) {
+    const [userData, setUserData] = React.useState(null);
+
+    React.useEffect(() => {
+        $.get('/get-user-data.json', (res) => {
+            setUserData(res);
+        })
+    }, []);
+    return (
+        <div id="edit-profile" className="center">
+            <h1 id="edit-profile-header" className="profile-headers">Edit Profile</h1>
+            {userData && <div>
+                <div>first name: {userData.fname}</div>
+                <div>last name: {userData.lname}</div>
+                <div>email: {userData.email}</div>
+                <div>phone: {userData.phone}</div>
+            </div>}
+
+        </div>
+    )
 }
 
 ReactDOM.render(
