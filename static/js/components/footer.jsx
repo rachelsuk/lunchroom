1fc00c27d8cae91e83ae92d9b4034d4679676be8
Rachel <rachelsuk97@gmail.com>
2021-06-16 07:17:59 +0000

    styled footer and removed attributions

--- static/js/components/footer.jsx
+++ static/js/components/footer.jsx
@@ -1,9 +1,17 @@
 function Footer(props) {
     return (
-        <div id="footer">
-            <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-            <div>Icons made by <a href="https://www.flaticon.com/authors/baianat" title="Baianat">Baianat</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-            <a href="https://www.vecteezy.com/free-vector/vector">Vector Vectors by Vecteezy</a>
+        // <div id="footer">
+
+        //     <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        //     <div>Icons made by <a href="https://www.flaticon.com/authors/baianat" title="Baianat">Baianat</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+        //     <a href="https://www.vecteezy.com/free-vector/vector">Vector Vectors by Vecteezy</a>
+        // </div>
+        <div id="wave-container">
+            <div id="wave"></div>
+            {/* <div id="footer-links">
+                <a id="about-app-link" href="/about-app">About this App</a>
+                <a id="credits-link" href="/credits">Credits</a>
+            </div> */}
         </div>
     );
 }
